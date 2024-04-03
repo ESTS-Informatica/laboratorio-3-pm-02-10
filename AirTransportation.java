e2b2a4adf0da5d2dc107282df6257c99df166e36
RicJSO <202300157@estudantes.ips.pt>
2024-04-03 11:19:16 +0100

    Nível 3 Concluido

--- AirTransportation.java
+++ AirTransportation.java
@@ -5,14 +5,17 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class AirTransportation
+public class AirTransportation extends Transport
 {
     private String name;
     private int numberOfContainers;
     
+    
     public AirTransportation(String name, int numberOfContainers){
+        super();
         this.name = name;
         this.numberOfContainers = numberOfContainers;
+        super.setFees(0.04);
     }
     
     public void setName(String name){
@@ -30,5 +33,17 @@ public class AirTransportation
     
     public int getNumberOfContainers(){
         return numberOfContainers;
+    }
+    
+    public double getPriceWithFees(){
+        return super.getPrice() * (1.0 + (super.getFees()));
+    }
+    
+    public String getTransportType(){
+        return "Transporte Aéreo";
+    }
+    
+    public String toString(){
+       return super.toString();
     }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -5,20 +5,34 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class GroundTransportation
+public class GroundTransportation extends Transport
 {
     private String licensePlate;
     
     public GroundTransportation(String licensePlate){
+        super();
+        super.setFees(0.03);
         this.licensePlate = licensePlate;
     }
     
-    public void setLicencePlate(String licencePlate){
-        if(licencePlate!=null)
-        this.licensePlate = licensePlate;
+    public void setLicensePlate(String licensePlate){
+        if(licensePlate!=null){
+        this.licensePlate = licensePlate;}
     }
     
-    public String getLicencePlate(){
+    public String getLicensePlate(){
         return licensePlate;
+    }
+    
+    public double getPriceWithFees(){
+        return super.getPrice() * (1.0 + (super.getFees()));
+    }
+    
+    public String getTransportType(){
+        return "Transporte Terrestre";
+    }
+    
+    public String toString(){
+       return super.toString();
     }
 }
